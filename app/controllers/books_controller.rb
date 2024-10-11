85ab677f59a0a81023e686cc014d3bff761e3cef
mark <mmarkmarkm0@gmail.com>
2024-10-12 01:39:37 +0300

    F_to add a new book:

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,23 @@ class BooksController < ApplicationController
   def show
     @book=Book.find(params[:id])
   end
+
+  def new
+    @book=Book.new
+  end
+
+  def create
+    @book=Book.create(book_params)
+
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author, :genre, :published_date)
+  end
 end
