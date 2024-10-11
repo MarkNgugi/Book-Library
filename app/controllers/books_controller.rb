80687acc24f2a0a6cfaa3dfbfbad3b0ad8a89c00
mark <mmarkmarkm0@gmail.com>
2024-10-12 01:48:52 +0300

    F_to edit a book:

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,20 @@ class BooksController < ApplicationController
     end
   end
 
+  def edit
+    @book=Book.find(params[:id])
+  end
+
+  def update
+    @book=Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :edit
+    end
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :author, :genre, :published_date)
